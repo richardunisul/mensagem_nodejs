01dee41ac4bdd1836db3759fb2f7b10dc32c1f50
richardunisul <richard.souza@animaeducacao.com.br>
2023-08-03 18:40:39 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express(); // Inicializa o servidor
+const port = 8000; //Define a porta do servidor
+
+app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.json());
+
+const rotas = require("./servicos"); // Rotas da mensagem
+app.use(rotas); // Carrega os serviços
+
+// Inicia o servidor
+
+app.listen(port, () =>{
+    console.log(`Servidor ouvindo porta ${port}`);
+});
