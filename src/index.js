2efab125735aed3c43037808e57a94928547bb23
richardunisul <103585524+richardunisul@users.noreply.github.com>
2023-08-03 20:11:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 8000; //Define a porta do servidor
 app.use(express.urlencoded({extended: true}))
 //app.use(cors());
 app.use(express.json());
-
+ 
 const rotas = require("./servicos"); // Rotas da mensagem
 app.use(rotas); // Carrega os serviços
 
